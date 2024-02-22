3ce45c4d86db8c48ce9f6ec1f186a2f63b7dddde
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-02-22 10:24:36 +0000

    Update solution_2.py

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -1,5 +1,10 @@
-# Read the input
-n = int(input())
+# Exercise 2 Solution: Squares Dictionary Generator
 
-# Calculate and print the next even number
-print(n + 2 - n % 2)
+# Prompt the user for an integer 'n'
+n = int(input("Enter an integer for 'n': "))
+
+# Create a dictionary comprehension that generates keys from 1 to 'n' and their squares as values
+squares_dict = {key: key**2 for key in range(1, n+1)}
+
+# Print the generated dictionary
+print(squares_dict)
