4330219747c5c72b20897de6e5cc8885f0580779
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-02-22 10:30:07 +0000

    Update solution_6.py

--- solutions/solution_6.py
+++ solutions/solution_6.py
@@ -1,9 +1,8 @@
-# Read the input
-a = int(input())
-b = int(input())
+# Prompt the user for a list of integers
+input_list = list(map(int, input("Enter a list of integers: ").split()))
 
-# Check if a is divisible by b
-divisible = "YES" * (a % b == 0) or "NO"
+# Remove duplicates by converting the list to a set, then convert back to a list and sort it
+unique_list = sorted(set(input_list))
 
-# Print the result
-print(divisible)
+# Print the resulting list without duplicates
+print(" ".join(map(str, unique_list)))
