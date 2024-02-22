e6d0a1e626ae93bbbd65e06cebd57ff771bd1435
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-02-22 10:31:33 +0000

    Update solution_7.py

--- solutions/solution_7.py
+++ solutions/solution_7.py
@@ -1,14 +1,20 @@
-# # Read the input
-n = int(input())
+# Prompt the user for a list of integers
+input_list = list(map(int, input("Enter a list of integers: ").split()))
 
-# Extract the digits
-d1 = n // 1000
-d2 = (n % 1000) // 100
-d3 = (n % 100) // 10
-d4 = n % 10
+# Initialize an empty dictionary to count occurrences of each value
+value_counts = {}
 
-# Calculate the sum of the digits
-sum_of_digits = d1 + d2 + d3 + d4
+# Populate the dictionary with values from the list
+for value in input_list:
+    # If the value is already in the dictionary, increment its count
+    if value in value_counts:
+        value_counts[value] += 1
+    # Otherwise, add the value to the dictionary with a count of 1
+    else:
+        value_counts[value] = 1
 
-# Print the sum of the digits
-print(sum_of_digits)
+# The number of unique values is the number of keys in the dictionary
+unique_count = len(value_counts.keys())
+
+# Print the number of unique values
+print(unique_count)
